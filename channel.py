4409967a1edc7ba8e92479a8566cd8be289c4d55
MKnie24 <mknierim@uni-osnabrueck.de>
2025-03-02 23:45:35 +0100

    Update channel.py

--- channel.py
+++ channel.py
@@ -22,11 +22,11 @@ app = Flask(__name__)
 app.config.from_object(__name__ + '.ConfigClass')  # configuration
 app.app_context().push()  # create an app context before initializing db
 
-HUB_URL = 'http://localhost:5555'
-HUB_AUTHKEY = '1234567890'
+HUB_URL = 'http://vm146.rz.uni-osnabrueck.de/hub'
+HUB_AUTHKEY = 'Crr-K24d-2N'
 CHANNEL_AUTHKEY = '0987654321'
 CHANNEL_NAME = "AI Multi Translation"
-CHANNEL_ENDPOINT = "http://localhost:5001"  # don't forget to adjust in the bottom of the file
+CHANNEL_ENDPOINT = "http://vm146.rz.uni-osnabrueck.de/u042/channel.wsgi"  # don't forget to adjust in the bottom of the file
 CHANNEL_FILE = 'messages.json'
 CHANNEL_TYPE_OF_SERVICE = 'aiweb24:chat'
 SUPPORTED_LANGUAGES = ["en", "fr", "de", "es", "it", "nl", "pt"]
